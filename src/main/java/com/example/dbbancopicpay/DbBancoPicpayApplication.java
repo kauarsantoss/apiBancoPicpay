7ef013092da20bd49df4f0bd9b82239887071b33
kauarsantoss <kaua.santos@germinare.org.br>
2024-08-20 12:02:57 -0300

    Atualização do application.properties

---
+++ src/main/java/com/example/dbbancopicpay/DbBancoPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbbancopicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBancoPicpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbBancoPicpayApplication.class, args);
+    }
+
+}
